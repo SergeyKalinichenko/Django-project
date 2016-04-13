117e793b4426494caab762f414ef55567e47394a
Sergey Kalinichenko <kalinichenko.serj@gmail.com>
2016-04-13 09:02:04 +0300

    add students to our project

--- students/views.py
+++ students/views.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,7 +7,24 @@ from django.http import HttpResponse
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students_list.html', {})
+    students = (
+        {'id': 1,
+            'first_name': u'Виталий',
+            'last_name': u'Подоба',
+            'ticket': 235,
+            'image': 'img/2.GIF'},
+        {'id': 2,
+            'first_name': u'Короста',
+            'last_name': u'Андрей',
+            'ticket': 2123,
+            'image': 'img/1.jpg'},
+        {'id': 3,
+            'first_name': u'Андрий',
+            'last_name': u'Притула',
+            'ticket': 5332,
+            'image': 'img/3.jpg'}
+            )
+    return render(request, 'students_list.html', {'students': students}) # В качестве значения передали переменную картежа с элементами словаря
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
